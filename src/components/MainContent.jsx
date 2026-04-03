b7c69e82b2d4e25a60a2330127300e89473c0560
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-04-04 01:21:18 +0800

    Make the footer font more lighter

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -82,7 +82,7 @@ function MainContent() {
           </div>
         </section>
         <footer className="mt-24 pt-8">
-          <p className="text-sm text-gray-500 text-center leading-relaxed">
+          <p className="text-sm text-gray-400 text-center leading-relaxed">
             Design inspired by Brittany Chiang, refined with my own personal
             touch. Built with React.js, Tailwind CSS, and Vite, and deployed
             through GitHub Pages. Crafted with care and attention to detail.
